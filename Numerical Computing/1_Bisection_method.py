5cae0f1fdfb613ac97706cf9719fe0a8d3bf3cf4
SFrehman <extrakashi5@gmail.com>
2026-06-15 22:54:50 +0500

    Update 1_Bisection_method.py

--- Numerical Computing/1_Bisection_method.py
+++ Numerical Computing/1_Bisection_method.py
@@ -2,16 +2,13 @@ def f(x):
     return (x**2) - 9 
 
 def bisection(a, b, n):
-    # Check if a root exists in the given interval first
     if f(a) * f(b) > 0:
         print("Solution in these roots does not exist")
         return
     
-    # Loop for up to number of iterations
     for i in range(1, n + 1):  # Starting from 1 makes the iteration printout cleaner
         mid = (a + b) / 2
         
-        # Check if mid is the exact root or close enough
         if f(mid) == 0:
             print(f"Iteration no {i:02d} :\t Exact Solution Found: {mid}")
             return
